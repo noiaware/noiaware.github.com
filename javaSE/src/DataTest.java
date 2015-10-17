dd539f7767ea749afd3b400003fdb3d96e8881f7
Administrator <noiaware@qq.com>
2015-10-17 16:52:52 +0800

    create javaSe

---
+++ javaSE/src/DataTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2015/10/17.
+ */
+//import java.io.*;
+//import java.lang.System;
+
+public class DataTest {
+    public static void main(String[] args) {
+        String a;
+        float f = 1.23f;
+        System.out.println(f);
+    }
+}
